c4fe6d3318780a1217f177b3bc663f599ee94085
PhelipeCopanski <phelipe.copanski@gmail.com>
2023-09-28 11:58:25 -0300

    começando a colocar peças no tabuleiro

--- Xadrez-console-project/Program.cs
+++ Xadrez-console-project/Program.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace Xadrez_console_project
 {
@@ -7,6 +8,10 @@ namespace Xadrez_console_project
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao {Linha = 0, Coluna = 0 });
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao { Linha = 1, Coluna = 3 });
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao { Linha = 2, Coluna = 4 });
 
             Tela.ImprimirTabuleiro(tab);
         }
--- Xadrez-console-project/Tabuleiro/Peca.cs
+++ Xadrez-console-project/Tabuleiro/Peca.cs
@@ -7,9 +7,9 @@
         public int QtdMovimetos { get; protected set; }
         public Tabuleiro Tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Cor cor, Tabuleiro tab)
         {
-            Posicao = posicao;
+            Posicao = null;
             Cor = cor;
             QtdMovimetos = 0;
             Tab = tab;
